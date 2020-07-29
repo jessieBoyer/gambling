b804eac2e423ab944613986a1d5f54b9b4301ea5
Jessie M Boyer <boyer_jessie@bah.com>
2020-07-29 19:30:51 -0400

    first commit.

---
+++ fightBetting.js
@@ -0,0 +1,26 @@
+let totalPot=0;
+let fighterPotA=0;
+let fighterPotB=2;
+
+
+function clearAllPots(){
+    totalPot=0;
+    fighterPotA=0;
+    fighterPotB=0;
+}
+
+function printTotalPot(){
+    console.log("The total pot is currently $ "+ totalPot);
+}
+
+function printFighterPotA(){
+    console.log("The total pot for fighterPotB is $" +fighterPotA);
+}
+
+function printFighterPotB(){
+    console.log("The total pot for fighterPotB is $" +fighterPotB);
+}
+
+
+printTotalPot();
+printFighterPotB();
