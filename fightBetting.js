71d9640860d3b7a5a1d7cb41299f019091815af1
Jessie M Boyer <boyer_jessie@bah.com>
2020-07-29 23:01:55 -0400

    next.\

--- fightBetting.js
+++ fightBetting.js
@@ -14,13 +14,31 @@ function printTotalPot(){
 }
 
 function printFighterPotA(){
-    console.log("The total pot for fighterPotB is $" +fighterPotA);
+    console.log("The total pot for fighterPotA is $" +fighterPotA);
 }
 
 function printFighterPotB(){
     console.log("The total pot for fighterPotB is $" +fighterPotB);
 }
 
+function addToPotA(amount){
+    fighterPotA=fighterPotA+amount;
+    totalPot=totalPot+fighterPotA;
+    console.log("Added a tottal of $ "+amount + " to fighter pot A");
+}
 
-printTotalPot();
+function addToPotB(amount){
+    fighterPotB=fighterPotB+amount;
+    totalPot=totalPot+fighterPotB;
+    console.log("Added a tottal of $ "+amount + " to fighter pot B");
+}
+
+addToPotA(100);
+addToPotB(150);
+printFighterPotA();
 printFighterPotB();
+printTotalPot();
+addToPotB(150);
+printFighterPotB();
+addToPotA(1000);
+printTotalPot();
